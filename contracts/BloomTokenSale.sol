364d4f06a02a968e30e7588eb5e636c5d98d0958
John Backus <johncbackus@gmail.com>
2017-10-01 15:41:56 -0700

    Make all vested token grants revokable

--- contracts/BloomTokenSale.sol
+++ contracts/BloomTokenSale.sol
@@ -91,7 +91,7 @@ contract BloomTokenSale is CappedCrowdsale, Ownable, TokenController, Pausable, 
 
     require(_amount <= 10 ** 25); // 10 million BLT. No presale partner will have more than this allocated. Prevent overflows.
 
-    token.grantVestedTokens(_receiver, _amount, uint64(now), cliffDate, vestingDate, false, false);
+    token.grantVestedTokens(_receiver, _amount, uint64(now), cliffDate, vestingDate, true, false);
 
     NewPresaleAllocation(_receiver, _amount);
   }
